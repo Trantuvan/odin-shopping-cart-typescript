ebf1025d40b0b774e6db80037f6af4ae43c103d6
trantuvan <trantuvan.kan@gmail.com>
2023-03-15 17:02:48 +0700

    Config HashRouter
    Because: gh-pages doesn't support browserRouter
    
    Config:

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 
 import "styles/normalize.css";
 import "styles/reset.css";
 import "styles/index.css";
 import App from "./App";
+import { HomePage } from "pages";
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [{ index: true, element: <HomePage /> }],
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
