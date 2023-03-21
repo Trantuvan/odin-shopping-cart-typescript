8f0293cf6eadf6b16f656e9af6f997378f775f34
trantuvan <trantuvan.kan@gmail.com>
2023-03-21 11:41:52 +0700

    remove dummy data

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -2,33 +2,9 @@ import clsx from "clsx";
 import { AiTwotoneStar } from "react-icons/ai";
 
 import styles from "styles/ProductItem.module.css";
-import { starRates } from "types";
+import { starRates, productItemProps } from "types";
 
-type productType = {
-  id: number;
-  title: string;
-  price: number;
-  description?: string;
-  category?: string;
-  image: string;
-  rating: {
-    rate: number;
-    count: number;
-  };
-};
-
-const ProductItem = () => {
-  const product: productType = {
-    id: 1,
-    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-    price: 109.95,
-    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    rating: {
-      rate: 1,
-      count: 120,
-    },
-  };
-
+function ProductItem({ product }: productItemProps) {
   // TODO: redo logic for render star rating
   const wholeStars: starRates = Array.from(
     { length: Math.floor(product.rating.rate) },
